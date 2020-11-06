79488b54803582e17737d89dcc759b421af1cf4b
premrobosoft <premnath@robosoftin.com>
2020-11-06 10:18:37 +0530

    Details page is desined to play Movie trailer

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 import { IMovies } from '../../../shared/imovies';
 import { MoviesService } from '../../services/movies.service';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-movie-details',
@@ -13,15 +15,20 @@ export class MovieDetailsComponent implements OnInit {
 
   movieId: any;
   movieDetails: IMovies;
+  trailerLink: SafeResourceUrl;
+
+  faStar = faStar;
 
   constructor(
     private route: ActivatedRoute,
     private moviesService: MoviesService,
-  ) { }
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
     this.movieId = this.route.snapshot.paramMap.get('id');
     this.movieDetails = this.moviesService.getMovie(this.movieId);
+    this.trailerLink = this.sanitizer.bypassSecurityTrustResourceUrl(this.movieDetails.trailerLink);
   }
   
 }
--- src/shared/moviesList.ts
+++ src/shared/moviesList.ts
@@ -7,7 +7,7 @@ export const moviesList: IMovies[] = [
         year: 2019,
         posterImage: "./assets/images/movi_poster/asn.jpeg",
         rating: 4.8,
-        trailerLink: ""
+        trailerLink: "https://www.youtube.com/embed/Dj-HNEZ59Z4"
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ export const moviesList: IMovies[] = [
         year: 2018,
         posterImage: "./assets/images/movi_poster/kgf.jpeg",
         rating: 4.9,
-        trailerLink: ""
+        trailerLink: "https://www.youtube.com/embed/-KfsY-qwBS0"
     },
     {
         id: 3,
@@ -23,39 +23,7 @@ export const moviesList: IMovies[] = [
         year: 2020,
         posterImage: "./assets/images/movi_poster/shivaji_surathkal.jpeg",
         rating: 4.5,
-        trailerLink: ""
-    },
-    {
-        id: 4,
-        title: "Yesa",
-        year: 2017,
-        posterImage: "./assets/images/movi_poster/yesa.jpeg",
-        rating: 4.2,
-        trailerLink: ""
-    },
-    {
-        id: 5,
-        title: "Girgit",
-        year: 2018,
-        posterImage: "./assets/images/movi_poster/girgit.jpeg",
-        rating: 4.3,
-        trailerLink: ""
-    },
-    {
-        id: 6,
-        title: "Chali Polilu",
-        year: 2012,
-        posterImage: "./assets/images/movi_poster/chalipolilu.jpeg",
-        rating: 4.9,
-        trailerLink: ""
-    },
-    {
-        id: 7,
-        title: "Appe Teacher",
-        year: 2016,
-        posterImage: "./assets/images/movi_poster/appeteacher.jpeg",
-        rating: 3.9,
-        trailerLink: ""
+        trailerLink: "https://www.youtube.com/embed/sl-Zm9r9HmY"
     },
     {
         id: 8,
@@ -63,7 +31,7 @@ export const moviesList: IMovies[] = [
         year: 2018,
         posterImage: "./assets/images/movi_poster/kesri.jpeg",
         rating: 4.7,
-        trailerLink: ""
+        trailerLink: "https://www.youtube.com/embed/JFP24D15_XM"
     },
     {
         id: 9,
@@ -71,6 +39,38 @@ export const moviesList: IMovies[] = [
         year: 2019,
         posterImage: "./assets/images/movi_poster/end_game.jpeg",
         rating: 4.6,
-        trailerLink: ""
-    }
+        trailerLink: "https://www.youtube.com/embed/TcMBFSGVi1c"
+    },
+    {
+        id: 10,
+        title: "Chali Polilu",
+        year: 2012,
+        posterImage: "./assets/images/movi_poster/chalipolilu.jpeg",
+        rating: 4.9,
+        trailerLink: "https://www.youtube.com/embed/ob4s1UXUsFc"
+    },
+    {
+        id: 11,
+        title: "Appe Teacher",
+        year: 2016,
+        posterImage: "./assets/images/movi_poster/appeteacher.jpeg",
+        rating: 3.9,
+        trailerLink: "https://www.youtube.com/embed/dD2MKZtIP38"
+    },
+    {
+        id: 12,
+        title: "Yesa",
+        year: 2017,
+        posterImage: "./assets/images/movi_poster/yesa.jpeg",
+        rating: 4.2,
+        trailerLink: "https://www.youtube.com/embed/le2_EsW6ago"
+    },
+    {
+        id: 13,
+        title: "Girgit",
+        year: 2018,
+        posterImage: "./assets/images/movi_poster/girgit.jpeg",
+        rating: 4.3,
+        trailerLink: "https://www.youtube.com/embed/PkRy7TZReZ8"
+    },
 ];

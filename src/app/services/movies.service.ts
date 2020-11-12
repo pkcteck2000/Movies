ef0060c6a6915ca389a2a006b876c81697335e37
premrobosoft <premnath@robosoftin.com>
2020-11-12 08:54:57 +0530

    Font-size of details changed

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -10,7 +10,7 @@ import { Md5 } from 'ts-md5/dist/md5';
 
 export class MoviesService {
 
-  movies: IMarvelMovies[];
+  //movies: IMarvelMovies[];
   movie: IMarvelMovies;
   existingFavoritMovieList;
 
@@ -18,15 +18,22 @@ export class MoviesService {
   pubKey = "756437395d731b7707a222bf7a943158";
   privKey = "b57c6d7e65b2344f97ac618a6036f83843a7a7e6";
   
+  ts = Date.now();
+  md5 = new Md5();
+  md5Hash = this.md5.appendStr(this.ts+this.privKey+this.pubKey);
+  finalUrl = `${this.baseUrl}${this.ts}&apikey=${this.pubKey}&hash=${this.md5Hash.end()}`;
+  movies: Observable<any> = this.httpClient.get(this.finalUrl);
 
   getMovieList = (): Observable<any>  => {
-    let ts = Date.now();
-    let md5 = new Md5();
-    let md5Hash = md5.appendStr(ts+this.privKey+this.pubKey);
-    let finalUrl = `${this.baseUrl}${ts}&apikey=${this.pubKey}&hash=${md5Hash.end()}`;
+    // let ts = Date.now();
+    // let md5 = new Md5();
+    // let md5Hash = md5.appendStr(ts+this.privKey+this.pubKey);
+    // let finalUrl = `${this.baseUrl}${ts}&apikey=${this.pubKey}&hash=${md5Hash.end()}`;
 
-    console.log(finalUrl);
-    return this.httpClient.get(finalUrl);
+    //console.log(finalUrl);
+    //return this.httpClient.get(finalUrl);
+    return this.movies;
+    
   }
 
   /*
